293bf81c2c8943d18374591e7178edd9fb23ccbf
quanizumi <quanizumi@noreply.gitcode.com>
2025-11-20 11:42:38 +0800

    新增了projectmatch界面

--- src/main/java/com/murasame/smarthrm/dao/EmployeeRepo.java
+++ src/main/java/com/murasame/smarthrm/dao/EmployeeRepo.java
@@ -1,15 +1,13 @@
 package com.murasame.smarthrm.dao;
 
-import com.murasame.smarthrm.entity.Project;
+import com.murasame.smarthrm.entity.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 /**
- * Project数据访问层
+ * Employee数据访问层
  * Spring Data MongoDB Repository接口
  */
 @Repository
-public interface ProjectRepo extends MongoRepository<Project, Integer> {
+public interface EmployeeRepo extends MongoRepository<Employee, Integer> {
 }
